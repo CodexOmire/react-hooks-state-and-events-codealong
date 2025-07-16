21e4c099592eae18b23c0c504edd3d1e37e65cab
Ezekiel Leslie Omire <ezekielomire@gmail.com>
2025-07-16 12:30:15 +0300

    Rendered Toggle component inside App for state and event demonstration

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
+// src/components/App.js
 import React from "react";
-import Toggle from "./components/Toggle";
+import Toggle from "./Toggle";
 
 function App() {
   return (
     <div>
-      <h1>React State and Events Lab</h1>
+      <h1>React State and Events Code-along</h1>
       <Toggle />
     </div>
   );
